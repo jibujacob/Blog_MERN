58185191a964bc598c4cc0d9ea97330b2de52d66
jibujacob <jibupjacob@gmail.com>
2021-12-08 23:24:04 +0530

    header section completed along with responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Topbar from "./components/Topbar/Topbar";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <Homepage/>
     </div>
   );
 }

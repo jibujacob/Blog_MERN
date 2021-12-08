24788586dd341e5c9fe8f1536208fc31fb388662
jibujacob <jibupjacob@gmail.com>
2021-12-08 17:44:54 +0530

    Navbar Section Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
+import Topbar from "./components/Topbar/Topbar";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Topbar/>
     </div>
   );
 }

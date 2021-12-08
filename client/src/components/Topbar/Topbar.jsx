865d515dc3d39b5975096592e5aa0e7fbc89d31c
jibujacob <jibupjacob@gmail.com>
2021-12-08 22:36:40 +0530

    Navbar responsiveness Completed

--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Topbar.css"
 
 function Topbar() {
+    // Added useState to control the hamberger menu state based on click
+    const [hamburgerMenuClicked,setHamburgerMenuClicked] = useState(false)
+
     return (
         <div className="topbar">
             <div className="topLeft">
@@ -11,7 +14,7 @@ function Topbar() {
                 <i className="topIcon fab fa-instagram-square"></i>
             </div>
             <div className="topCenter">
-                <ul className="topList">
+                <ul className={hamburgerMenuClicked ? "topList topListMobile-active" : "topList" }>
                     <li className="topListItem">HOME</li>
                     <li className="topListItem">ABOUT</li>
                     <li className="topListItem">CONTACT</li>
@@ -23,6 +26,15 @@ function Topbar() {
                 <img className="topImg" src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="user_profile" />
                 <i className  ="topSearchIcon fas fa-search"></i>
             </div>
+            {
+                <div className="topHamburgerMenu" onClick={()=>{
+                    setHamburgerMenuClicked(oldValue => !oldValue)
+                }}>
+                    <div className={hamburgerMenuClicked ? "line1 toggle" : "line1"}></div>
+                    <div className={hamburgerMenuClicked ? "line2 toggle" : "line2"}></div>
+                    <div className={hamburgerMenuClicked ? "line3 toggle" : "line3"}></div>
+                </div>
+            }
         </div>
     )
 }

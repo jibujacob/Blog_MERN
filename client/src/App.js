6dd8c186d0a8449524bea899512da79305d69d0b
jibujacob <jibupjacob@gmail.com>
2021-12-09 23:15:46 +0530

    Login and Register pages done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import Topbar from "./components/Topbar/Topbar";
 import Homepage from "./pages/Homepage/Homepage";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import Settings from "./pages/Settings/Settings";
 import Single from "./pages/Single/Single";
 import Write from "./pages/Write/Write";
@@ -11,7 +13,9 @@ function App() {
       {/* <Homepage/> */}
       {/* <Single/> */}
       {/* <Write/> */}
-      <Settings/>
+      {/* <Settings/> */}
+      {/* <Login/> */}
+      <Register/>
     </div>
   );
 }
